0af1aed35da5168f1ac2f3f3bf5ecc91ff414e45
Natalie Clark <nj_clark@hotmail.co.uk>
2023-09-26 10:20:03 +0100

    Add LetterCounter class

---
+++ lib/letter_counter.py
@@ -0,0 +1,34 @@
+class LetterCounter:
+    def __init__(self, text):
+        self.text = text
+
+    def calculate_most_common(self):
+        counter = {}
+        most_common = None
+        most_common_count = 0
+        for char in self.text:
+            if not char.isalpha():
+                continue
+            # Original code:
+            # keyscounter[char] = counter.get(char, 1) + 1        
+            # Debugged by adding if/else statement
+            #   For counter dictionary keys that don't yet exist, their value is set to 1
+            #   For counter dictionary keys that already exist, their value is increased by 1
+            if counter.get(char) == None:
+                counter[char] = 1
+            else:
+                counter[char] = counter.get(char) + 1
+
+            if counter[char] > most_common_count:
+                most_common = char
+                # Original code:
+                # most_common_count += counter[char]  
+                # Debugged by changing to =
+                most_common_count = counter[char]
+        return [most_common_count, most_common]
+
+
+counter = LetterCounter("Digital Punk")
+print(counter.calculate_most_common())
+# Intended output:
+# [2, "i"]
